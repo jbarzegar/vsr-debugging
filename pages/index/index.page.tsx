152e291f8e46b3c67d1e06ae480db8f51de39a82
James Barzegar <james.barzegar@agilebits.com>
2023-08-04 16:30:32 -0400

    initial commit

---
+++ pages/index/index.page.tsx
@@ -0,0 +1,22 @@
+import { Counter } from "./Counter";
+
+export { Page };
+
+function Page() {
+  return (
+    <>
+      <h1>Welcome</h1>
+      This page is:
+      <ul>
+        <li>Rendered to HTML.</li>
+        <li>
+          Interactive. <Counter />
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export const meta = {
+  title: "this is a custom title",
+};
